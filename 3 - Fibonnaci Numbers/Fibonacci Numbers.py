ac73442d32dde369cb75a3e7328515e4fdd8fd0c
ArticWabbit <mirzamaeen69@gmail.com>
2020-09-05 21:43:31 +0600

    Initial Fibonacci Numbers Commit

---
+++ 3 - Fibonnaci Numbers/Fibonacci Numbers.py
@@ -0,0 +1,18 @@
+while True:
+    def fib(n):
+        if n < 0:
+            print("Incorrect input")
+
+        elif n == 0:
+            return 0
+
+        elif n == 1:
+            return 1
+        else:
+            return fib(n - 1) + fib(n - 2)
+
+
+    n = int(input("n = "))
+    print(fib(n))
+
+    continue
