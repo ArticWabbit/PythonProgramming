9d7eed3321821ae38207e16f6f9a4dd624e28914
ArticWabbit <mirzamaeen69@gmail.com>
2020-09-11 13:55:08 +0600

    fixed git error

--- 4 - Number Checker/Number Checker.py
+++ 4 - Number Checker/Number Checker.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD:4 - Number Checker/Number Checker.py
+
 def checker(num):
     if num % 2 == 0:
         print("Number is even\n")
@@ -16,22 +16,4 @@ while True:
         print('Invalid Input\n')
 
     continue
-=======
-def checker(num):
-    if num % 2 == 0:
-        print("Number is even\n")
-    else:
-        print("Number is odd\n")
 
-
-print("Even/Odd Number Checker\n")
-
-while True:
-    try:
-        num = input('Input Number: ')
-        checker(int(num))
-    except ValueError:
-        print('Invalid Input\n')
-
-    continue
->>>>>>> a509bd990291189bbdd0a33be3065fd529867c9b:Number Checker/Number Checker.py
